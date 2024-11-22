b5327235eb981b534a9d5d21a5432bef93b39169
arslan-ashraf <arslanash@gmail.com>
2024-11-22 14:09:08 -0500

    base file added

---
+++ base.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    // run command: g++ <filename>.cpp -o <output_filename>.exe
+    std::cout << "g++ compiler working" << std::endl;
+
+    // run command: clang++ <filename>.cpp -o <output_filename>.exe
+    std::cout << "clang++ compiler working" << std::endl;
+    return 0;
+}
