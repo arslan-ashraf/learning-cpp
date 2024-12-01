9b071388137526bcd345d424dee5c317341233d8
arslan-ashraf <arslanash@gmail.com>
2024-11-30 19:54:50 -0500

    changes

--- cpp-syntax.cpp
+++ cpp-syntax.cpp
@@ -401,7 +401,7 @@ void some_function(void (*pointer_callback_test)(int), int x){
 
 int main(){
 
-    // void (*p)(int) = &callback_test;
+    // void (*pointer_to_function)(int) = &callback_test;
     void (*pointer_to_function)(int) = callback_test;
     
     some_function(pointer_to_function, 0);
